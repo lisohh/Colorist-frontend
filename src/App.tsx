41789cadfbf4f03f456b2f8bb5c19625f9df5ac5
Taehee Kim <taehee.kim@sap.com>
2022-07-14 21:46:40 +0900

    upgrade pallete and flex wrap

--- src/App.tsx
+++ src/App.tsx
@@ -16,18 +16,29 @@ function RoundButton({ hex, addSelected }: { hex: string, addSelected: (hex: str
 
 const BOX_COUNT = 10;
 const WHITE_HEX = '#ffffff';
+const pallete = [
+  '#e6e6e6', 
+  '#d2b48c', 
+  '#800000', 
+  '#7e181e',
+  '#29f9ff', 
+  '#a8a8f8', 
+  '#5050f1', 
+  '#ffb3ba',
+  '#ccff00', 
+  '#ff7f50', 
+  '#fa8072', 
+  '#bada55',
+];
 
 function App() {
-  const pallete = ["#ff0000", "#00ff00", "#0000ff"];
-
-  const [selectedColors, setSelectedColors] = React.useState(Array(BOX_COUNT).fill(WHITE_HEX));
+  const [selectedColors, setSelectedColors] = React.useState<string[]>(Array(BOX_COUNT).fill(WHITE_HEX));
   function addSelected(hex: string){
     setSelectedColors(old => {
       const firstWhiteIndex = old.findIndex(hex => hex === WHITE_HEX)
       if(firstWhiteIndex === -1){
         return old;
       }
-      console.log('firstWhiteIndex', firstWhiteIndex)
 
       const copy = [...old];
       copy[firstWhiteIndex] = hex;
@@ -46,19 +57,19 @@ function App() {
   return (
     <div className="App">
       <article>
-        <div className="selectedContainer flexRow">
+        <div className="selectedContainer flex flex-row">
           {selectedColors
             .map((hex, index) => (
               <div
+                key={index}
                 className="selectedBox"
                 style={{backgroundColor: hex}}
                 onClick={() => { removeSelected(index) }}>
-              
               </div>
             ))}
         </div>
-        <div id="pallete" className="flexRow">
-          {pallete.map(hex => <RoundButton hex={hex} addSelected={addSelected} />)}
+        <div id="pallete" className="flex flex-row flex-wrap">
+          {pallete.map(hex => <RoundButton key={hex} hex={hex} addSelected={addSelected} />)}
         </div>
       </article>
     </div>

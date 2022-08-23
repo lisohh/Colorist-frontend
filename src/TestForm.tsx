8323d50ad30f986f3ff7c13cc392105a2b87fa5d
lisohh <sohyunlee0902@gmail.com>
2022-08-23 20:45:35 +0900

    Update: TestForm 수정

--- src/TestForm.tsx
+++ src/TestForm.tsx
@@ -1,23 +1,11 @@
 import * as React from "react";
 import Pallete from "./pallete";
 import "./TestForm.css";
+import QuizList from "./quizList.json";
 
 // 8월 16일 2번
 // https://www.radix-ui.com/docs/primitives/components/tabs#examples
 // bun add @radix-ui/react-tabs
-const QuizList = [
-  "문제1번 내용",
-  "문제2번 내용",
-  "문제3번 내용",
-  "문제4번 내용",
-  "문제5번 내용",
-  "문제6번 내용",
-  "문제7번 내용",
-  "문제8번 내용",
-  "문제9번 내용",
-  "문제10번 내용",
-];
-
 // 탭의 색깔에 명도/채도가 반영되면 좋겠다
 
 function TestForm() {
@@ -39,12 +27,13 @@ function TestForm() {
     <article className="test-form">
       <div className="quiz-paragraph">
         <a className="category" href="">
-          2022년도 - 제품
+          {/* {QuizList.quiz[0].class} */}
+          "2022년도 1회차 기출"
         </a>
         <h1 className="text-2xl mb-2">
-          Q.{" "}
+          Q.
           {
-            "따뜻하고 풍요로운 이미지와 차갑고 쓸쓸한 이미지 두가지를 연출해 보세요."
+            /* {QuizList[0].quizContent} */ "따뜻하고 풍요로운 이미지와 차갑고 쓸쓸한 이미지 두가지를 연출해 보세요."
           }
         </h1>
         <h2 id="condition" className="text-lg">
@@ -55,7 +44,9 @@ function TestForm() {
       {/* 8번 input은 text 줄바꿈이 안되므로 textarea를 사용해 서술칸을 만든다. */}
       {/* <input type="text" /> */}
       <p id="allColor">주보강 기입</p>
-      <label htmlFor="main-color">주조색</label>
+      <label className="color-label" htmlFor="main-color">
+        주조색
+      </label>
       <input
         id="color-selection"
         placeholder="주조색:"
@@ -66,21 +57,25 @@ function TestForm() {
         // https://beta.reactjs.org/learn/reacting-to-input-with-state
         onChange={(event) => setMainColor(event.target.value)}
       />
-      <label htmlFor="sub-color">보조색</label>
+      <label className="color-label" htmlFor="sub-color">
+        보조색
+      </label>
       <input
         id="color-selection"
         placeholder="보조색:"
         value={subColor}
         onChange={(event) => setSubColor(event.target.value)}
       />
-      <label htmlFor="point-color">강조색</label>
+      <label className="color-label" htmlFor="point-color">
+        강조색
+      </label>
       <input
         id="color-selection"
         placeholder="강조색:"
         value={pointColor}
         onChange={(event) => setPointColor(event.target.value)}
       />
-      <label htmlFor="explanation" id="explanation">
+      <label className="color-label" htmlFor="explanation" id="explanation">
         배색 설명
       </label>
       <textarea
@@ -90,28 +85,34 @@ function TestForm() {
         onChange={(event) => setExplanation(event.target.value)}
       />
       <p id="allColor">주보강 기입</p>
-      <label htmlFor="main-color">주조색</label>
+      <label className="color-label" htmlFor="main-color">
+        주조색
+      </label>
       <input
         id="color-selection"
         placeholder="주조색:"
         value={mainColor}
         onChange={(event) => setMainColor(event.target.value)}
       />
-      <label htmlFor="sub-color">보조색</label>
+      <label className="color-label" htmlFor="sub-color">
+        보조색
+      </label>
       <input
         id="color-selection"
         placeholder="보조색:"
         value={subColor}
         onChange={(event) => setSubColor(event.target.value)}
       />
-      <label htmlFor="point-color">강조색</label>
+      <label className="color-label" htmlFor="point-color">
+        강조색
+      </label>
       <input
         id="color-selection"
         placeholder="강조색:"
         value={pointColor}
         onChange={(event) => setPointColor(event.target.value)}
       />
-      <label htmlFor="explanation" id="explanation">
+      <label className="color-label" htmlFor="explanation" id="explanation">
         배색 설명
       </label>
       <textarea

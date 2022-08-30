58a849e054dac8558ed727ede39ead09ec20f088
lisohh <sohyunlee0902@gmail.com>
2022-08-30 12:05:08 +0900

    Add: extension col 설정 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateColumns: {
+        14: "repeat(14, minmax(0, 1fr))",
+      },
+    },
   },
   plugins: [require("daisyui")],
 };

d6e57a37a3c5ded0829cc06467bde0c2b6fd5e7a
lisohh <sohyunlee0902@gmail.com>
2022-09-08 21:34:45 +0900

    Update: react-table 빼고 라우팅 수정

--- src/index.tsx
+++ src/index.tsx
@@ -10,22 +10,25 @@ import Login from "./pages/login/Login";
 import TestForm from "./pages/testform/TestForm";
 import Mypage from "./pages/mypage/Mypage";
 import Qlists from "./pages/qlists/Qlists";
+import ResizableBoxes from "./pages/testform/ResizableBoxes";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Drawer>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<TestForm />} />
-          <Route path="/login/*" element={<Login />} />
-          <Route path="/mypage/*" element={<Mypage />} />
-          <Route path="/qlists/*" element={<Qlists />} />
-          {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-          <Route path="*" element={<Notfound />}></Route>
-        </Routes>
-      </Drawer>
-    </BrowserRouter>
+    <ResizableBoxes />
   </React.StrictMode>,
   document.getElementById("root")
 );
+
+// <BrowserRouter>
+// <Drawer>
+//   <Nav />
+//   <Routes>
+//     <Route path="/" element={<TestForm />} />
+//     <Route path="/login/*" element={<Login />} />
+//     <Route path="/mypage/*" element={<Mypage />} />
+//     <Route path="/qlists/*" element={<Qlists />} />
+//     {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
+//     <Route path="*" element={<Notfound />}></Route>
+//   </Routes>
+// </Drawer>
+// </BrowserRouter>

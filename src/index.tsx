4a709c7359d5762ac0a7e8d0f0ab79481da80e96
lisohh <sohyunlee0902@gmail.com>
2022-12-29 09:30:03 +0900

    Fix: path 수정

--- src/index.tsx
+++ src/index.tsx
@@ -11,8 +11,8 @@ import Register from "./pages/register/Register";
 import TestForm from "./pages/testform/TestForm";
 import Mypage from "./pages/mypage/Mypage";
 import Qlists from "./pages/qlists/Qlists";
-import ResizableBoxes from "./pages/testform/ResizableBoxes";
 import Root from "./pages/root/Root";
+import ProfileEdit from "./pages/mypage/profileEdit/ProfileEdit";
 
 const rootContainer = document.getElementById("root")!;
 
@@ -26,16 +26,14 @@ root.render(
         <Routes>
           <Route path="/" element={<Root />} />
 
-          <Route path="/quiz/:quizId" element={<TestForm />} />
-          {/* /problem/:problemId */}
-          {/* /problem/1 */}
-          {/* /problem/2 */}
-          <Route path="/login/*" element={<Login />} />
-          <Route path="/register/*" element={<Register />} />
-          <Route path="/mypage/*" element={<Mypage />} />
-          <Route path="/qlists/*" element={<Qlists />} />
+          <Route path="/quizs" element={<Qlists />} />
+          <Route path="/quizs/:quizId" element={<TestForm />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/mypage" element={<Mypage />} />
+          <Route path="/mypage/edit" element={<ProfileEdit />} />
           {/* 상단에 위치하는 라우트들의 규칙을 모두 확인, 일치하는 라우트가 없는경우 처리 */}
-          <Route path="*" element={<Notfound />}></Route>
+          <Route path="*" element={<Notfound />} />
         </Routes>
       </Drawer>
     </BrowserRouter>

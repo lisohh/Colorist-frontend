d2985d3d0e0fa9249bb6ae4b6fa8f1fa222f3b11
lisohh <sohyunlee0902@gmail.com>
2023-01-07 11:34:54 +0900

    Feat: Footer 컴포넌트 분리

---
+++ src/components/Footer.tsx
@@ -0,0 +1,17 @@
+function Footer() {
+  return (
+    <footer className="footer footer-center content-end mt-28 p-10 bg-base-200 text-base-content rounded">
+      <div>
+        <div className="grid grid-flow-col gap-4">
+          <a>lisohh</a>
+          <a>mimik</a>
+          <a>TamjungTokki</a>
+        </div>
+      </div>
+      <div>
+        <p>Copyright © 2022 - All right reserved by Team Mipokki</p>
+      </div>
+    </footer>
+  );
+}
+export default Footer;

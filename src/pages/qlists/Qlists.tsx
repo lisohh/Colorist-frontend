591d3bfdc2eb3804562ec95d37d419a5db424aa0
lisohh <sohyunlee0902@gmail.com>
2023-01-20 15:51:52 +0900

    Fix: 모든 문제만 남기고, table 컴포넌트 사용, table-fixed로 css수정

--- src/pages/qlists/Qlists.tsx
+++ src/pages/qlists/Qlists.tsx
@@ -5,123 +5,49 @@ import SelectGroup from "../../components/SelectGroup";
 import { Link } from "react-router-dom";
 import Button from "~/components/Button";
 import useProblemList from "~/hooks/useProblemList";
+import Table from "~/components/Table";
 
 // const data2 = Array(10).fill(["2022년도", "1회차", "1-2", "문제 제목 얼레벌레 블라블라", "2022.7.23",])
 
 function Mypage() {
   const problemList = useProblemList();
   return (
-    <article className="Qpage">
-      <h2 className="title text-center text-3xl my-12">문제 목록</h2>
-      <section className="view-arrange"></section>
-      <div className="most-viewed-list">
-        <h3 className="mostViewed-Q-list-heading ml-4 mb-4 text-xl font-semibold">
-          가장 많이 본 문제
-        </h3>
-        <table
-          className="table table-zebra w-full my-4"
-          aria-labelledby="Mostviewd-list-heading"
-        >
-          <thead>
-            <tr>
-              <th>선택</th>
-              <th>연도</th>
-              <th>회차</th>
-              <th>유형</th>
-              <th>문제 이름</th>
-              <th>푼 날짜</th>
+    <article className="Qpage flex flex-col gap-4">
+      <h2 className="title text-center text-4xl my-12">문제 목록</h2>
+      <SelectGroup />
+      <Table
+        headList={["선택", "연도", "회차", "유형", "문제 제목", "푼 날짜"]}
+        headRatio={[
+          "w-1/12",
+          "w-1/12",
+          "w-1/12",
+          "w-1/12",
+          "w-7/12",
+          "w-2/12 lg:w-1/12",
+        ]}
+        dataList={problemList}
+        getKey={(problem) => problem.id}
+        zebra="purple"
+        className="table-fixed"
+        Row={({ data: { id, year, round, type, title, solvedAt } }) => {
+          return (
+            <tr className="text-center text-xs lg:text-base">
+              <th className="px-1 lg:px-2">
+                <input type="checkbox" className="checkbox checkbox-" />
+              </th>
+              <td className="px-1 lg:px-2">{year}</td>
+              <td className="px-1 lg:px-2">{round}</td>
+              <td className="px-1 lg:px-2">{type}</td>
+
+              <td className="px-1 lg:px-2 truncate break-words">
+                <Link to={`/quizs/${id}`}>{title}</Link>
+              </td>
+
+              <td className="px-1 lg:px-2">{solvedAt}</td>
             </tr>
-          </thead>
-          <tbody>
-            {problemList.slice(0, 5).map((item, i) => (
-              <tr key={i}>
-                <Link to={"/quizs/" + item.id} className="contents">
-                  <td>
-                    <input type="checkbox" className="checkbox checkbox-" />
-                  </td>
-                  <td>{item.year}</td>
-                  <td>{item.round}</td>
-                  <td>{item.type}</td>
-                  <td>{item.title}</td>
-                  <td>{item.solvedAt}</td>
-                </Link>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <section className="save-arrange"></section>
-      <div className="most-solved-list">
-        <h1 className="mostSolved-Q-list-heading gap-4 ml-4 mb-4 text-xl font-semibold">
-          가장 많이 푼 문제
-        </h1>
-        <table
-          className="table table-zebra w-full my-4"
-          aria-labelledby="Mostsolved-list-heading"
-        >
-          <thead>
-            <tr>
-              <th>선택</th>
-              <th>연도</th>
-              <th>회차</th>
-              <th>유형</th>
-              <th>문제 이름</th>
-              <th>푼 날짜</th>
-            </tr>
-          </thead>
-          <tbody>
-            {problemList.slice(0, 5).map((item) => (
-              <tr>
-                <td>
-                  <input type="checkbox" className="checkbox checkbox-" />
-                </td>
-                <td>{item.year}</td>
-                <td>{item.round}</td>
-                <td>{item.type}</td>
-                <td>{item.title}</td>
-                <td>{item.solvedAt}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-      <section className="all">
-        <div className="question-list">
-          <h3 className="all-Q-list-heading ml-4 mb-4 text-xl font-semibold">
-            모든 문제
-          </h3>
-          <SelectGroup />
-          <table
-            className="table table-zebra w-full mb-8"
-            aria-labelledby="solved-list-heading"
-          >
-            <thead>
-              <tr>
-                <th>선택</th>
-                <th>연도</th>
-                <th>회차</th>
-                <th>유형</th>
-                <th>문제 이름</th>
-                <th>푼 날짜</th>
-              </tr>
-            </thead>
-            <tbody>
-              {problemList.map((item) => (
-                <tr>
-                  <td>
-                    <input type="checkbox" className="checkbox checkbox-" />
-                  </td>
-                  <td>{item.year}</td>
-                  <td>{item.round}</td>
-                  <td>{item.type}</td>
-                  <td>{item.title}</td>
-                  <td>{item.solvedAt}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-        </div>
-      </section>
+          );
+        }}
+      />
       <footer className="flex flex-row justify-between">
         <Button>출력</Button>
         <Pagination />
@@ -132,3 +58,19 @@ function Mypage() {
 }
 
 export default Mypage;
+function invariant(
+  problem:
+    | {
+        year: string;
+        round: string;
+        type: string;
+        title: string;
+        condition: string;
+        solvedAt: string;
+        id: number;
+      }
+    | undefined,
+  arg1: string
+) {
+  throw new Error("Function not implemented.");
+}
